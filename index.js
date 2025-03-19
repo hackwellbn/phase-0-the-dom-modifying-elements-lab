fa31f45512fd8d76cdf6ce4650bf55c8abb6e704
hackwellbn <119762901+hackwellbn@users.noreply.github.com>
2025-03-19 05:23:42 +0000

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+const mainElement = document.getElementById("main");
+mainElement.remove();  // Removes the <main> element
+
+const newHeader = document.createElement("h1");
+
+newHeader.id = "victory";
+
+newHeader.textContent = "YOUR-NAME is the champion"; // Replace 'YOUR-NAME' with your actual name
+
+document.body.append(newHeader);
